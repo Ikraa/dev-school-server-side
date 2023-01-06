5d8b61a00c513f31c4bc4cc362b2127b8dc9cf69
munnisaikra <munnisaikra750@gmail.com>
2023-01-07 03:25:01 +0600

    course api added

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express=require("express")
+const data=require('./data/Courses.json')
 const app=express()
-
+const cors=require('cors')
+app.use(cors())
 const port=process.env.PORT || 4000 
 
 
 
 app.get("/",(req,res)=>{
     res.send("Dev school server start successfully...")
+})
+app.get("/courses",(req,res)=>{
+    res.send({staus:true,data:data})
+})
+app.get("/courses/:id",(req,res)=>{
+    const id=req.params.id
+    const course=data.find(item=>item.id=== +id)
+    res.send(course)
+    // res.send({staus:true,data:data})
 })
 
 
